af248fcde607002abca4725ec4b6b266e90a812e
al1928 <shafievalbert@mail.ru>
2020-10-16 12:25:53 +0300

    up AShRateService.php

--- ratesrv/AShRateService.php
+++ ratesrv/AShRateService.php
@@ -1,11 +1,11 @@
 <?php
     function getRates($rate) {
-        $Rates = file_get_contents('http://api.currencyfreaks.com/latest?apikey=94c578ee776d4b789dd3339c7db2abd4&base=USD');
+        $Rates = file_get_contents("http://api.currencyfreaks.com/latest?apikey=94c578ee776d4b789dd3339c7db2abd4&base={$rate}");
         $jsonRates = json_decode($Rates);
         $convertRate = $jsonRates->rates->RUB;
         return $convertRate;
     }
-    $rate = "USD";
+    $rate = "EUR";
     $dollar_rate = getRates($rate);
     $now = date("H:i:s")
 ?>
