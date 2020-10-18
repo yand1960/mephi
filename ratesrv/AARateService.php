f633cd7239fc39e1737b01262d3bbacdf2a5af54
Aleksey <alekseyarkhipov1@yandex.ru>
2020-10-19 00:37:32 +0400

    file adddd by aa

--- ratesrv/AARateService.php
+++ ratesrv/AARateService.php
@@ -1,15 +1,11 @@
 <?php
-	$ch = curl_init();
+$ch = curl_init('http://www.cbr-xml-daily.ru/daily_json.js');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-	curl_setopt($ch, CURLOPT_URL, 'http://www.cbr-xml-daily.ru/daily_json.js');
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$json = curl_exec($ch);
+curl_close($ch);
 
-	$json = curl_exec($ch);
-	curl_close($ch);
+$result = json_decode($json, true);
 
-	$result = json_decode($json, true);
-	$return_value = array();
-	$return_value['time'] = date('Y-m-d H:i:s');
-	$return_value['rate'] = $result['Valute']['USD']['Value'];
-	header('Content-Type: application/json');
-	echo json_encode($return_value);
+header('Content-Type: application/json');
+echo json_encode(['time' => time(), 'result' => $result['Valute']['USD']['Value'] ]);
