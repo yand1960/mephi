7d467b3d8acaf032590a1e9cef5fdaf20a6f64ac
levkin <l89105277279@gmail.com>
2020-10-20 21:09:40 +0300

    HW

--- ratesrv/PLRateService.php
+++ ratesrv/PLRateService.php
@@ -1,16 +1,20 @@
 <?php
-function get_course() {
+function get_currencies() {
     $xml = simplexml_load_file('http://cbr.ru/scripts/XML_daily.asp');
-    $course = array();
+    $currencies = array();
     foreach ($xml->xpath('//Valute') as $valute) {
-        $course[(string)$valute->CharCode] = (float)str_replace(',', '.', $valute->Value);  
+        $currencies[(string)$valute->CharCode] = (float)str_replace(',', '.', $valute->Value);  
     }
-    return $course;
+    return $currencies;
 }
 
-// Курс валют 
-$course = get_course();
-print "<h3>Курс доллара:</h3>";
-echo $course['USD'];  
-print "<h3>Курс евро:</h3>";
-echo $course['EUR'];	
+// Курс доллара
+$dollar = get_currencies();
+$course = $dollar['USD'];
+
+//Время и Курс в формате JSON 
+$date = date("H:i:s");
+header('Content-Type: application/json');
+
+echo "{\"Время\": \"$date\" 
+ \"Курс $\": \"$course\"}";
