b7b563cf44c9af566617628f082dbaa82b3d3f6c
primo <ilyatitovrussia@gmail.com>
2020-11-07 14:07:28 +0300

    ITDev_Distances

---
+++ distances/ITDev_NearestShopServise.php
@@ -0,0 +1,10 @@
+<?php
+
+$shops = [
+    ["address" => "Tinkoff", "latitude" => 64, "longitude" => 44],
+    ["address" => "Niva", "latitude" => 58, "longitude" => 81],
+    ["address" => "Moscow", "latitude" => 55, "longitude" => 37],
+    ["address" => "Oceania", "latitude" => 37, "longitude" => 55]
+];
+echo json_encode($shops);
+
